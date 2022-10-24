ec983221532afe082e2742ca1222a62d7bc1015f
Felipe Calais <felipe_calais@hotmail.com>
2022-10-24 14:12:48 -0300

    Atualização

---
+++ 9_estruturarepeticao/while.js
@@ -0,0 +1,16 @@
+/*var rept = 0
+
+while (rept <=10)
+{
+    console.log(rept+' Sou gostoso')
+    rept++
+}*/
+
+
+var arr = ['eu','sou',1,'gostosao']
+var y = 0
+
+while (y < 4){
+    console.log(arr[y])
+    y++
+}
