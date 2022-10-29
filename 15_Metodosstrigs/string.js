297f2b1ad304ce934e8c8739f2cae339d81b9b44
Felipe Calais <felipe_calais@hotmail.com>
2022-10-29 13:32:39 -0300

    novos exercicios

---
+++ 15_Metodosstrigs/string.js
@@ -0,0 +1,15 @@
+var nome = 'felipe'
+
+console.log(nome.length)    // Numero de caracteres
+
+console.log(nome.indexOf('l')) // informa onde está a letra L dentro da variavel 
+
+
+var frase = "eu vi tres tigres tristes"
+
+//Slice tira palavra de dentro de uma variavel e armazena em outra
+
+var tigres = frase.slice(11,17)  
+
+console.log(tigres)
+
