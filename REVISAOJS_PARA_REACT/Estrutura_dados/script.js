7dccb4f336553b98cef767d5026c2a21e3ed520a
Felipe Calais <felipe_calais@hotmail.com>
2023-01-16 21:17:35 -0300

    revisao js

---
+++ REVISAOJS_PARA_REACT/Estrutura_dados/script.js
@@ -0,0 +1,16 @@
+//Arrays
+
+
+//formas de se criar um array:
+var list = new Array
+var arr = []
+var arrNovo = ['',,0]
+
+var arr2 = [2,4,6,78,201]
+
+
+for( el in arr2){
+    console.log(arr2[el])
+}
+
+console.log(arrNovo)
