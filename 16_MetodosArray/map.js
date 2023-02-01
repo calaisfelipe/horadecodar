841235f9e14a5c0d2cc5f20194d463976be8d9c7
Felipe Calais <felipe_calais@hotmail.com>
2023-02-01 15:30:27 -0300

    new class

---
+++ 16_MetodosArray/map.js
@@ -0,0 +1,12 @@
+const arr = [2,4,65,90,'lets']
+const arr2 = [23,55,22,1232]
+
+
+//Método MAP - REALIZA UMA OPERACAO EM CADA ELEMENTO DO ARRAY E RETORNA UM NOVO ARRAY COM O RESULTADO
+const result = arr.map((numb) => numb+'@')
+console.log(result)
+
+//ForEach
+arr.forEach( (el) =>{
+    console.log(el)
+})
